3d8c1d5d373d1213f25dcdc5860615cb0d347263
joseph <developergitch@outlook.com>
2018-10-21 17:28:15 +0300

    Update server ip issues in EmailService

--- src/main/scala/com/joseph/dao/services/EmailService.scala
+++ src/main/scala/com/joseph/dao/services/EmailService.scala
@@ -1,7 +1,5 @@
 package com.joseph.dao.services
 
-import java.net.{InetAddress, UnknownHostException}
-
 import com.joseph.domain.User
 import org.springframework.beans.factory.annotation.{Autowired, Value}
 import org.springframework.mail.SimpleMailMessage
@@ -22,17 +20,17 @@ class EmailService @Autowired()(jms: JavaMailSender) {
 
     var text = "Hello " + user.getName + ",\n" + "Please click the link below to confirm your email address. Ignore if you did not request.\n"
     //http://ip/userId/email
-    var link = ""
-    try
-      link = "http://" + InetAddress.getLocalHost.getHostAddress
-    catch {
-      case e: UnknownHostException =>
-        e.printStackTrace()
-        link = serverAddress
-    }
+//    var link = ""
+//    try
+//      link = "http://" + InetAddress.
+//    catch {
+//      case e: UnknownHostException =>
+//        e.printStackTrace()
+//        link = serverAddress
+//    }
 
-    link = link + "/activate/" + user.getId + "/" + user.getEmail
-    text = text + link
+    serverAddress = serverAddress + "/activate/" + user.getId + "/" + user.getEmail
+    text = text + serverAddress
     simpleMailMessage.setText(text)
     jms.send(simpleMailMessage)
   }
